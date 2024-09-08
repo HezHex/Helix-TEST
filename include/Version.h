3b4576dc1d2c85529a701a7ed905d49576f5a06a
HezHex <46758942+HezHex@users.noreply.github.com>
2024-09-08 18:04:22 +0200

    Add files via upload

---
+++ include/Version.h
@@ -0,0 +1,9 @@
+
+    // AUTO GENERATED FILE, DO NOT EDIT
+    #ifndef VERSION
+        #define VERSION "0.1.353"
+    #endif
+    #ifndef BUILD_TIMESTAMP
+        #define BUILD_TIMESTAMP "2024-09-08 18:01:59.013110"
+    #endif
+    
